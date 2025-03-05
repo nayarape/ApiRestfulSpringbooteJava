4833cecefeb15bc5369c0e91e12adf79db7a0cae
Nayara Peixoto Pires <106106145+nayarape@users.noreply.github.com>
2025-03-05 20:48:24 -0300

    Add files via upload

---
+++ decola/src/test/java/me/dio/decola/DecolaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.decola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
